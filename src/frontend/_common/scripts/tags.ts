063f8c77ffacb24f4de8e7db50744a5242fbf7a4
AvatarKage <contact@avatarkage.com>
2026-09-13 20:13:25 -0400

    moved recommendedTags to single source

---
+++ src/frontend/_common/scripts/tags.ts
@@ -0,0 +1,15 @@
+export const recommendedTags = [
+    { tag: "action", name: "Action" },
+    { tag: "adventure", name: "Adventure" },
+    { tag: "comedy", name: "Comedy" },
+    { tag: "crime", name: "Crime" },
+    { tag: "drama", name: "Drama" },
+    { tag: "fantasy", name: "Fantasy" },
+    { tag: "historical", name: "Historical" },
+    { tag: "horror", name: "Horror" },
+    { tag: "mystery", name: "Mystery" },
+    { tag: "romance", name: "Romance" },
+    { tag: "sci-fi", name: "Sci-Fi" },
+    { tag: "sports", name: "Sports" },
+    { tag: "war", name: "War" }
+];
